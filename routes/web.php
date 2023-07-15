8568c55f52bc0d2fda502f1348227060cbb01b13
Rizki Madya <rizkimadya88@gmail.com>
2023-07-15 13:25:43 +0800

    one to many

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// relasi one to one untuk table user dan phone
+Route::get('/', [UserController::class, 'index']);
 
-Route::get('/users', [UserController::class, 'index']);
+// relasi one to many untuk table post dan comment
+Route::get('/posts', [PostController::class, 'index']);
